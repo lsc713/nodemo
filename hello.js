e316bb11bd00b1af1833622314a0c0ac0b2b726b
lsc713 <lsc7134@naver.com>
2024-06-10 12:15:38 +0900

    feat: node.js api test

--- hello.js
+++ hello.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname+'/public'))
+app.set('view engine', 'ejs')
+
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://admin:???@cluster0.iykywxd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+new MongoClient(url).connect().then((client)=>{
+    console.log('DB연결성공')
+    db = client.db('posting')
+
+    app.listen(8083, () => {
+        console.log('http://localhost:8080 에서 서버 실행중')
+    })
+
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.listen(8080, ()=>{
     console.log('http://localhost:8080')
@@ -12,6 +29,16 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/news',(req,res)=>{
-    res.send('news')
+    db.collection('post').insertOne({title : 'hello'})
+})
+
+app.get('/list',async (req,res)=>{
+    let result = await db.collection('post').find().toArray()
+    console.log(result)
+    res.render('list.ejs',{posts : result})
+})
+
+app.get('/time', (req,res)=>{
+    res.render('date.ejs',{data : new Date()})
 })
 
