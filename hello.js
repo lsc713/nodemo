7ec104923e7ce10f2b90a93945b3742c908a83fd
lsc713 <lsc7134@naver.com>
2024-06-12 15:16:23 +0900

    feat: paging with nextButton 요청

--- hello.js
+++ hello.js
@@ -95,3 +95,15 @@ app.delete('/delete', async (req, res) => {
     let result = await db.collection('post').deleteOne({ _id : new ObjectId(req.body.id)})
     res.send('delete Complete')
 });
+
+app.get('/list/:id',async (req,res)=>{
+    let result = await db.collection('post').find().skip((req.params.id-1)*5).limit(5).toArray()
+    res.render('list.ejs',{posts : result})
+})
+
+app.get('/list/next/:id',async (req,res)=>{
+    let result = await db.collection('post')
+        .find({_id : {$gt : new ObjectId(req.params.id)}})
+        .limit(5).toArray()
+    res.render('list.ejs',{posts : result})
+})
